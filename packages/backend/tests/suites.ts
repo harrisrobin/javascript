c72a9f504c13bdb18b221dfb31220303726b7c51
Dimitris Klouvas <dimitris@clerk.dev>
2023-03-31 18:32:13 +0300

    chore(backend): Extract jwt audience assertion & add unit tests
    Also there was some changes in the code:
    - aud claim with empty values does NOT throw verification error

--- packages/backend/tests/suites.ts
+++ packages/backend/tests/suites.ts
@@ -6,6 +6,7 @@ import keysTest from './dist/tokens/keys.test.js';
 import pathTest from './dist/util/path.test.js';
 import verifyTest from './dist/tokens/verify.test.js';
 import verifyJwtTest from './dist/tokens/jwt/verifyJwt.test.js';
+import jwtAssertionsTest from './dist/tokens/jwt/assertions.test.js';
 
 import utilRequestTest from './dist/util/request.test.js';
 import factoryTest from './dist/api/factory.test.js';
@@ -16,6 +17,7 @@ import exportsTest from './dist/exports.test.js';
 const suites = [
   apiTest,
   exportsTest,
+  jwtAssertionsTest,
   requestTest,
   utilRequestTest,
   keysTest,
